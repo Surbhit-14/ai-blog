a399878afd9bfb58f67f11f23850af180269ca23
surbhit <surbhitsrivastava29@gmail.com>
2024-07-19 14:05:02 +0530

    https://github.com/Surbhit-14/chat-app.git

---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit, signal, } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { GeminiService } from './gemini.service';
+import { interval } from 'rxjs';
+import { HeaderComponent } from "./compponnents/header/header.component";
+import { FooterComponent } from "./compponnents/footer/footer.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, CommonModule, HeaderComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  title = 'AI-Blog-WS';
+
+  count : number = 0;
+  promt : string = '';
+  prompt : string[] = ["write a blog in 500 without any heading just in paragraphs words about any one New Technology News today "];
+  lenght : number = this.prompt.length;
+  geminiService : GeminiService = inject(GeminiService);
+  blogData :any[] = [];
+  constructor(){
+   this.geminiService.resend().subscribe((res) => {
+    if(res){
+     this.blogData.push(res);
+    }
+   })
+  }
+  ngOnInit(): void {
+    const obs$ = interval(86400000);
+    obs$.subscribe((d) => {
+      console.log(d);
+      this.sendData(d % this.lenght);
+      this.count = d;
+    })
+    }
+    
+    sendData(d: number){
+    if(this.prompt[d]){
+      this.geminiService.generateText(this.prompt[d]);
+    }
+  }
+}
---
+++ src/app/gemini.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GeminiService {
+
+  private gai : GoogleGenerativeAI;
+  private text : string = '';
+  private mess : BehaviorSubject<any> = new BehaviorSubject(null);
+  constructor() {
+    this.gai = new GoogleGenerativeAI("AIzaSyASkv_R_LLjpwhjyvk1OcUdtw33ur-qriE");
+  }
+
+  async generateText(promt: string){
+    const model = this.gai.getGenerativeModel({model: "gemini-pro"});
+    const result = await model.generateContent(promt);
+    const response = await result.response;
+    this.text = response.text();
+    console.log(this.text);
+    this.mess.next({mes : this.text});
+    }
+
+    public resend() : Observable<any> {
+      return this.mess.asObservable();
+    }
+}
